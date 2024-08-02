9468700062c2a814eb5e69fc94109a214d70f04d
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-03 04:53:32 +0530

    changes done

--- src/app/module/acr/acr-login/acr-login.component.ts
+++ src/app/module/acr/acr-login/acr-login.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcrLoginComponent implements OnInit {
 
+  password = 'password';
+    currentPassword = 'password';
+    confirmPassword = 'password';
+    showPassword = false;
+    showConfPassword = false;
+    showCurrentPassword = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public showHidePass(): void {
+    if (this.password === 'password') {
+        this.password = 'text';
+        this.showPassword = true;
+    } else {
+        this.password = 'password';
+        this.showPassword = false;
+    }
+}
 
 }
