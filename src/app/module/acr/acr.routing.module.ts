71f660b07d00789529809e92a4b7d5a20b9e2894
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-02 16:52:08 +0530

    changes done

--- src/app/module/acr/acr.routing.module.ts
+++ src/app/module/acr/acr.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcrComponent } from './acr.component';
 import { AcrFormComponent } from './acr-form/acr-form.component';
+import { AcrTableComponent } from './acr-table/acr-table.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
       {
         path: "acr-form",
         component: AcrFormComponent
+      },
+      {
+        path: "acr-table",
+        component: AcrTableComponent
       },
     ]
   },
