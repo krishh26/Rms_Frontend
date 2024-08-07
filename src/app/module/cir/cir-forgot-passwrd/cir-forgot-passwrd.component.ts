db33664ae5bc428d1fe59a5a8b6e921bb99b94fc
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-08 04:47:17 +0530

    changes done

---
+++ src/app/module/cir/cir-forgot-passwrd/cir-forgot-passwrd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cir-forgot-passwrd',
+  templateUrl: './cir-forgot-passwrd.component.html',
+  styleUrls: ['./cir-forgot-passwrd.component.css']
+})
+export class CirForgotPasswrdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/module/cir/cir-otherdetails-form/cir-otherdetails-form.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { CirSericeService } from 'src/app/services/cir-service/cir-serice.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+
+@Component({
+  selector: 'app-cir-otherdetails-form',
+  templateUrl: './cir-otherdetails-form.component.html',
+  styleUrls: ['./cir-otherdetails-form.component.css']
+})
+export class CirOtherdetailsFormComponent implements OnInit {
+  password = 'password';
+  showPassword = false;
+  confirmPassword = 'password';
+  confirmShowPassword = false;
+
+  file: any;
+  constructor(
+    private cirSericeService: CirSericeService,
+    private notificationService: NotificationService,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  // Number only validation
+  NumberOnly(event: any): boolean {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+  
+  public showHidePass(type: string): void {
+    if (type == 'password' && this.password === 'password') {
+      this.password = 'text';
+      this.showPassword = true;
+    } else {
+      this.password = 'password';
+      this.showPassword = false;
+    }
+
+    if (type !== 'password' && this.confirmPassword === 'password') {
+      this.confirmPassword = 'text';
+      this.confirmShowPassword = true;
+    } else {
+      this.confirmPassword = 'password';
+      this.confirmShowPassword = false;
+    }
+  }
+
+  fileUpload(event: any): void {
+    const file = event.target.files[0];
+    const data = new FormData();
+    data.append('files', file || '');
+
+    this.cirSericeService.fileUpload(data).subscribe((response) => {
+      if (response?.status) {
+        this.file = response?.data;
+        this.notificationService.showSuccess(response?.message || 'File successfully uploaded.')
+      } else {
+        this.notificationService.showError(response?.message || 'File not uploaded.')
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message || 'File not uploaded.')
+    })
+  }
+
+}
