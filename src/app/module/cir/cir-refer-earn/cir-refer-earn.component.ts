81e3668de6af9f5bd32101efe78e1bdaf8fc730c
darshan <darshandumaraliya@gmail.com>
2024-08-14 02:13:10 +0530

    Changes done.

--- src/app/module/cir/cir-refer-earn/cir-refer-earn.component.ts
+++ src/app/module/cir/cir-refer-earn/cir-refer-earn.component.ts
@@ -1,10 +1,10 @@
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { CirSericeService } from 'src/app/services/cir-service/cir-serice.service';
-import { Patterns } from 'src/app/shared/constant/validation-patterns.const';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-cir-refer-earn',
@@ -12,23 +12,22 @@ import { LocalStorageService } from 'src/app/services/local-storage/local-storag
   styleUrls: ['./cir-refer-earn.component.css']
 })
 export class CirReferEarnComponent implements OnInit {
-  referForm!: FormGroup;
+  skillsForm: FormGroup;
   loginUser: any = [];
-  referredBy : string ='';
+  referredBy: string = '';
 
   constructor(
     private cirSericeService: CirSericeService,
     private notificationService: NotificationService,
     private router: Router,
     private localStorageService: LocalStorageService,
+    private cdr: ChangeDetectorRef,
+    private fb: FormBuilder
   ) {
-   // this.loginUser = this.localStorageService.getLogger();
-    this.referForm = new FormGroup({
-      name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.pattern(Patterns.email)]),
-      job_title: new FormControl('', [Validators.required]),
+    // this.loginUser = this.localStorageService.getLogger();
+    this.skillsForm = this.fb.group({
+      skills: this.fb.array([]),
     });
-
   }
 
   ngOnInit() {
@@ -36,22 +35,53 @@ export class CirReferEarnComponent implements OnInit {
     this.referredBy = this.loginUser?.referredBy
   }
 
-  submit() {
-    this.referForm.markAllAsTouched();
-    if (!this.referForm.valid) {
-      return this.notificationService.showError('Please fill all details');
-    }
+  get skills(): FormArray | any {
+    return this.skillsForm.get('skills') as FormArray;
+  }
 
-    this.cirSericeService.referAndEarn(this.referForm.value).subscribe((response) => {
-      if (response?.status) {
-        this.notificationService.showSuccess('Refer and Earn Successful');
-        this.router.navigate(['/cir/cir-refer-earn-thank-you']);
-        this.referForm.reset();
-      } else {
-        return this.notificationService.showError('User not refer');
-      }
-    }, (error) => {
-      return this.notificationService.showError(error?.message || 'User not refer');
-    });
+  newSkill(): FormGroup {
+    return this.fb.group({
+      name: '',
+      email: '',
+      jobTitle: ''
+    })
+  }
+
+  addSkills() {
+    console.log(this.skills)
+    this.skills.push(this.newSkill());
+    this.cdr.markForCheck();
+  }
+
+  trackByFn(index: number, skill: any): number {
+    return index;
+  }
+
+  removeSkill(i: number) {
+    this.skills.removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.skillsForm.value);
+  }
+
+  submit() {
+    console.log(this.skillsForm.value);
+    // this.referForm.markAllAsTouched();
+    // if (!this.referForm.valid) {
+    //   return this.notificationService.showError('Please fill all details');
+    // }
+
+    // this.cirSericeService.referAndEarn(this.referForm.value).subscribe((response) => {
+    //   if (response?.status) {
+    //     this.notificationService.showSuccess('Refer and Earn Successful');
+    //     this.router.navigate(['/cir/cir-refer-earn-thank-you']);
+    //     this.referForm.reset();
+    //   } else {
+    //     return this.notificationService.showError('User not refer');
+    //   }
+    // }, (error) => {
+    //   return this.notificationService.showError(error?.message || 'User not refer');
+    // });
   }
 }
