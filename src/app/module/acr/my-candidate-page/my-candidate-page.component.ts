8514c0b5c9ca612d6c0e79ddda4f85ea52b89381
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-22 01:37:19 +0530

    changes done

--- src/app/module/acr/my-candidate-page/my-candidate-page.component.ts
+++ src/app/module/acr/my-candidate-page/my-candidate-page.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCandidatePageComponent implements OnInit {
 
+  candidates = [
+    { name: 'kishan', email: 'kishan@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'ayush', email: 'ayush@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'gaurang', email: 'gaurang@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+  ];
+
   constructor() { }
 
   ngOnInit() {
