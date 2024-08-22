93a12541bc27b9b57033d0efe467a9a53ad54598
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-23 01:33:42 +0530

    changes done

--- src/app/module/acr/cir-all-jobs/cir-all-jobs.component.ts
+++ src/app/module/acr/cir-all-jobs/cir-all-jobs.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import * as moment from 'moment';
+import { AcrServiceService } from 'src/app/services/acr-service/acr-service.service';
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { Patterns } from 'src/app/shared/constant/validation-patterns.const';
 
 @Component({
   selector: 'app-cir-all-jobs',
@@ -6,10 +13,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cir-all-jobs.component.css']
 })
 export class CirAllJobsComponent implements OnInit {
-
-  constructor() { }
+  agencyForm!: FormGroup;
+  constructor(
+    private router: Router,
+    private notificationService: NotificationService,
+    private localStorageService: LocalStorageService,
+    private acrservice: AcrServiceService,
+  ) {
+    this.agencyForm = new FormGroup({
+      jobtitle: new FormControl('', [Validators.required, Validators.pattern(Patterns.name)]),
+      Noofroles: new FormControl('', [Validators.required]),
+      positionstartdate: new FormControl('', [Validators.required]),
+      publishdate: new FormControl(moment(new Date()).format('dd-MM-YYYY'), [Validators.required, Validators.pattern(Patterns.name)]),
+      clientname: new FormControl('', [Validators.required]),
+      location: new FormControl('', [Validators.required, Validators.pattern(Patterns.email)]),
+      dayrate: new FormControl('', [Validators.required]),
+    });
+  }
 
   ngOnInit() {
+  }
+
+  submit() {
+
+  }
+
+  // Number only validation
+  NumberOnly(event: any): boolean {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
   }
 
 }
