782cec8f7cd05e19ba898dcec143184ee97a1e70
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-23 02:09:16 +0530

    changes done

--- src/app/module/acr/cir-all-jobs/cir-all-jobs.component.ts
+++ src/app/module/acr/cir-all-jobs/cir-all-jobs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
 import { AcrServiceService } from 'src/app/services/acr-service/acr-service.service';
@@ -14,12 +14,22 @@ import { Patterns } from 'src/app/shared/constant/validation-patterns.const';
 })
 export class CirAllJobsComponent implements OnInit {
   agencyForm!: FormGroup;
+  file: any;
+  resourcesForm!: FormGroup
   constructor(
     private router: Router,
     private notificationService: NotificationService,
     private localStorageService: LocalStorageService,
     private acrservice: AcrServiceService,
+    private fb: FormBuilder,
   ) {
+
+    this.resourcesForm = this.fb.group({
+      candidates: this.fb.array([
+        this.createCandidateFormGroup()
+      ])
+    });
+
     this.agencyForm = new FormGroup({
       jobtitle: new FormControl('', [Validators.required, Validators.pattern(Patterns.name)]),
       Noofroles: new FormControl('', [Validators.required]),
@@ -38,6 +48,27 @@ export class CirAllJobsComponent implements OnInit {
 
   }
 
+  get candidates(): FormArray {
+    return this.resourcesForm.get('candidates') as FormArray;
+  }
+
+  createCandidateFormGroup(): FormGroup {
+    return this.fb.group({
+      howmanyresources: ['', Validators.required],
+      cv: ['', [Validators.required]],
+      nationality: ['', Validators.required],
+      location: ['', Validators.required]
+    });
+  }
+
+  addCandidate() {
+    this.candidates.push(this.createCandidateFormGroup());
+  }
+
+  removeCandidate(index: number) {
+    this.candidates.removeAt(index);
+  }
+
   // Number only validation
   NumberOnly(event: any): boolean {
     const charCode = event.which ? event.which : event.keyCode;
@@ -45,6 +76,25 @@ export class CirAllJobsComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  fileUpload(event: any): void {
+    const file = event.target.files[0];
+    const data = new FormData();
+    data.append('files', file || '');
+
+    this.acrservice.fileUpload(data).subscribe((response) => {
+      if (response?.status) {
+        this.file = response?.data;
+        console.log(this.file);
+
+        this.notificationService.showSuccess(response?.message || 'File successfully uploaded.')
+      } else {
+        this.notificationService.showError(response?.message || 'File not uploaded.')
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message || 'File not uploaded.')
+    })
   }
 
 }
