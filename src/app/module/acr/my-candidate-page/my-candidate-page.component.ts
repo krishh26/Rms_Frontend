c714d36b2961da48fba8d4973f1882a318dfab29
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-25 00:22:21 +0530

    changes done

--- src/app/module/acr/my-candidate-page/my-candidate-page.component.ts
+++ src/app/module/acr/my-candidate-page/my-candidate-page.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class MyCandidatePageComponent implements OnInit {
 
   candidates = [
-    { name: 'kishan', email: 'kishan@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'ayush', email: 'ayush@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'gaurang', email: 'gaurang@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
-    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Active' },
+    { name: 'kishan', email: 'kishan@gmail.com', joiningDate: '21-aug-2024', status: 'Active' ,contractenddate : '21-sep-2024'},
+    { name: 'ayush', email: 'ayush@gmail.com', joiningDate: '21-aug-2024', status: 'Active' ,contractenddate : '21-sep-2024'},
+    { name: 'gaurang', email: 'gaurang@gmail.com', joiningDate: '21-aug-2024', status: 'Inactive' ,contractenddate : '21-sep-2024'},
+    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Terminated' ,contractenddate : '21-sep-2024'},
+    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Inactive' ,contractenddate : '21-sep-2024'},
+    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' ,contractenddate : '21-sep-2024'},
+    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Terminated' ,contractenddate : '21-sep-2024'},
+    { name: 'mihir', email: 'mihir@gmail.com', joiningDate: '21-aug-2024', status: 'Active' ,contractenddate : '21-sep-2024'},
+    { name: 'raj', email: 'raj@gmail.com', joiningDate: '21-aug-2024', status: 'Terminated' ,contractenddate : '21-sep-2024'},
   ];
 
   constructor() { }
