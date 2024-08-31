5b953e91e12fa90ab87c97a7cefd7b601b96ede8
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-31 19:52:23 +0530

    changes done

--- src/app/module/acr/acr.routing.module.ts
+++ src/app/module/acr/acr.routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcrComponent } from './acr.component';
 import { AcrFormComponent } from './acr-form/acr-form.component';
-import { AcrTableComponent } from './acr-table/acr-table.component';
 import { AcrLoginComponent } from './acr-login/acr-login.component';
-import { AcrCardComponent } from './acr-card/acr-card.component';
 import { AuthGuard } from 'src/app/shared/auth-guard/auth.guard';
 import { AcrProfileComponent } from './acr-profile/acr-profile.component';
 import { AcrAccordianCardComponent } from './acr-accordian-card/acr-accordian-card.component';
@@ -28,24 +26,18 @@ const routes: Routes = [
     component: AcrAdminComponent
   },
   {
+    path: "acr-accordian-card",
+    // canActivate: [AuthGuard],
+    component: AcrAccordianCardComponent
+  },
+  {
     path: '',
     component: AcrComponent,
     children: [
-
-      {
-        path: "acr-card",
-        // canActivate: [AuthGuard],
-        component: AcrCardComponent
-      },
       {
         path: "acr-profile",
         // canActivate: [AuthGuard],
         component: AcrProfileComponent
-      },
-      {
-        path: "acr-accordian-card",
-        // canActivate: [AuthGuard],
-        component: AcrAccordianCardComponent
       },
       {
         path: "acr-candidate-page",
