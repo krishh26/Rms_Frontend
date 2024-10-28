8192747a940960314c205b2d2984447adb1fba79
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-10-29 02:29:44 +0530

    changes done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,16 @@
+// export const environment = {
+// 	production: false,
+
+// 	url: "http://localhost:4200",
+// 	//  baseUrl : 'http://192.168.1.7:4100',
+// 	baseUrl: 'http://82.112.230.8:5010/api/v1/'
+//   };
+
+
 export const environment = {
 	production: false,
-  
+
 	url: "http://localhost:4200",
 	//  baseUrl : 'http://192.168.1.7:4100',
-	baseUrl: 'http://82.112.230.8:5010/api/v1/'
-  };
-  
+	baseUrl: 'https://api.saivensolutions.co.uk/api/v1'
+};
