f8218032fcdac5a605d825a6ffb5312fed38b4c5
darshan <darshandumaraliya@gmail.com>
2025-05-30 12:41:31 +0530

    Implement edit card functionality.

--- src/app/module/cir/create-future-card/create-future-card.component.ts
+++ src/app/module/cir/create-future-card/create-future-card.component.ts
@@ -1,30 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CirSericeService } from 'src/app/services/cir-service/cir-serice.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import Swal from 'sweetalert2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create-future-card',
   templateUrl: './create-future-card.component.html',
   styleUrls: ['./create-future-card.component.css']
 })
-export class CreateFutureCardComponent {
+export class CreateFutureCardComponent implements OnInit {
 
   futureCards: any[] = [];
   showModal: boolean = true;
   inputValue: string = '';
   showLoader: boolean = false;
-
+  isSubmitting: boolean = false;
+  editCardData: any = {
+    _id: '',
+    name: ''
+  };
 
   constructor(
     private cirservice: CirSericeService,
     private notificationService: NotificationService,
+    private modalService: NgbModal,
+    private toastr: ToastrService,
   ) { }
 
   ngOnInit() {
     this.getFuturecard();
   }
-
 
   getFuturecard() {
     this.cirservice.getFutureCard().subscribe((response) => {
@@ -39,30 +46,22 @@ export class CreateFutureCardComponent {
   }
 
   createCard() {
-    if (this.inputValue) {
-
-      this.cirservice.createFutureCard({ name: this.inputValue }).subscribe((response) => {
-        if (response && response.data) {
-          this.inputValue = '';
-          this.getFuturecard();
-        }
-      }, (error) => {
-        console.log('error', error);
-      });
-
+    if (!this.inputValue.trim()) {
+      this.toastr.warning('Please enter a name');
+      return;
     }
-  }
 
-  // deleteCard(event: MouseEvent, id: string) {
-  //   event.stopPropagation();
-  //   this.cirservice.deleteFutureCard(id).subscribe((response) => {
-  //     if (response && response.data) {
-  //       this.getFuturecard();
-  //     }
-  //   }, (error) => {
-  //     console.log('error', error);
-  //   });
-  // }
+    this.cirservice.createFutureCard({ name: this.inputValue }).subscribe((response) => {
+      if (response && response.data) {
+        this.inputValue = '';
+        this.getFuturecard();
+        this.toastr.success('Card created successfully');
+      }
+    }, (error) => {
+      console.log('error', error);
+      this.toastr.error('Error creating card');
+    });
+  }
 
   deleteCard(event: MouseEvent, id: string) {
     event.stopPropagation();
@@ -82,6 +81,7 @@ export class CreateFutureCardComponent {
             this.showLoader = false;
             this.notificationService.showSuccess('Card successfully deleted');
             this.getFuturecard();
+            this.toastr.success('Card deleted successfully');
           } else {
             this.showLoader = false;
             this.notificationService.showError(response?.message);
@@ -104,6 +104,7 @@ export class CreateFutureCardComponent {
         this.showLoader = false;
         this.notificationService.showSuccess(isActive ? 'Card activated successfully' : 'Card deactivated successfully');
         this.getFuturecard(); // Refresh the list to get updated data
+        this.toastr.success('Status updated successfully');
       } else {
         this.showLoader = false;
         this.notificationService.showError(response?.message);
@@ -118,4 +119,41 @@ export class CreateFutureCardComponent {
     });
   }
 
+  openEditModal(modal: any, card: any) {
+    this.editCardData = {
+      _id: card._id,
+      name: card.name
+    };
+    this.modalService.open(modal, { centered: true });
+  }
+
+  closeModal() {
+    this.modalService.dismissAll();
+    this.editCardData = {
+      _id: '',
+      name: ''
+    };
+  }
+
+  updateCard() {
+    if (!this.editCardData.name.trim()) {
+      this.toastr.warning('Please enter sa name');
+      return;
+    }
+
+    this.isSubmitting = true;
+    this.cirservice.updateFutureCardStatus(this.editCardData._id, { name: this.editCardData.name }).subscribe({
+      next: (response) => {
+        this.toastr.success('Card updated successfully');
+        this.closeModal();
+        this.getFuturecard();
+      },
+      error: (error) => {
+        this.toastr.error('Error updating card');
+      },
+      complete: () => {
+        this.isSubmitting = false;
+      }
+    });
+  }
 }
