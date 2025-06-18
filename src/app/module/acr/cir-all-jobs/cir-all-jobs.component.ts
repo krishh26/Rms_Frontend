b20fb997a6c6e668d2e8107ba840041e6cc23c69
darshan <darshandumaraliya@gmail.com>
2025-06-19 01:32:55 +0530

    changes done.

--- src/app/module/acr/cir-all-jobs/cir-all-jobs.component.ts
+++ src/app/module/acr/cir-all-jobs/cir-all-jobs.component.ts
@@ -32,7 +32,7 @@ export class CirAllJobsComponent implements OnInit {
   @ViewChild('uploadcvModal') uploadcvModal: any;
   public timerSubscription: Subscription = new Subscription()
   selectedStatus: string = '';
-  statusList: string[] = ['Active','Inactive', 'Actioned', 'Under Review', 'Expired', 'Accepted', 'Not Submitted'];
+  statusList: string[] = ['Active','Inactive', 'Actioned', 'Under Review', 'Expired', 'Not Submitted'];
   page: number = pagination.page;
   pagesize = pagination.itemsPerPage;
   totalRecords: number = pagination.totalRecords;
