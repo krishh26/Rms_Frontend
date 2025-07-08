27ba3a406ca6801115bdd425fc7ddb7ae11507b7
krishh26 <kishansinhparmar999@gmail.com>
2025-07-08 16:48:25 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from './services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rms';
+  loginUser: any;
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnInit() {
+    const tokenData = localStorage.getItem("DecodedToken");
+    if (tokenData) {
+      try {
+        this.loginUser = JSON.parse(tokenData);
+        console.log('Decoded Token in App:', this.loginUser);
+      } catch (error) {
+        console.error("Error parsing token data as JSON", error);
+      }
+    }
+  }
 }
